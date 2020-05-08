05f5c3062fd25f79d5d5d7dff36cad2bfed028a3
Jacopo Carlini <jack1395@hotmail.it>
2020-05-08 12:04:37 +0200

    test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,21 +1,36 @@
-//// This is a basic Flutter widget test.
-////
-//// To perform an interaction with a widget in your test, use the WidgetTester
-//// utility that Flutter provides. For example, you can send tap and scroll
-//// gestures. You can also use WidgetTester to find child widgets in the widget
-//// tree, read text, and verify that the values of widget properties are correct.
+// This is a basic Flutter widget test.
 //
-//import 'package:flutter/material.dart';
-//import 'package:flutter_test/flutter_test.dart';
-//
-//import 'package:calendaroo/main.dart';
-//
-//void main() {
-//  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-//    // Build our app and trigger a frame.
-//    await tester.pumpWidget(MyApp());
-//
-//    // Verify that our counter starts at 0.
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:calendaroo/redux/middlewares/app.middlewares.dart';
+import 'package:calendaroo/redux/middlewares/calendar.middlewares.dart';
+import 'package:calendaroo/redux/reducers/app.reducer.dart';
+import 'package:calendaroo/redux/states/app.state.dart';
+import 'package:calendaroo/services/initializer-app.service.dart';
+import 'package:calendaroo/services/shared-preferences.service.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:redux/redux.dart';
+
+import 'package:calendaroo/main.dart';
+
+void main() {
+  testWidgets('Navigate NewEvent test', (WidgetTester tester) async {
+//    await setUpEnv();
+    var store = createStore();
+//    initializerAppService.preLoadingData(store);
+
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MyApp(
+      store: store,
+    ));
+
+    expect(find.text('April 2020'), findsNothing);
+
+    // Verify that our counter starts at 0.
 //    expect(find.text('0'), findsOneWidget);
 //    expect(find.text('1'), findsNothing);
 //
@@ -26,5 +41,16 @@
 //    // Verify that our counter has incremented.
 //    expect(find.text('0'), findsNothing);
 //    expect(find.text('1'), findsOneWidget);
-//  });
-//}
+  });
+}
+
+Future<void> setUpEnv() async {
+  await sharedPreferenceService.getSharedPreferencesInstance();
+  await sharedPreferenceService.setString('environment', 'develop');
+}
+
+Store<AppState> createStore() {
+  return Store(appReducer,
+      initialState: AppState.initial(),
+      middleware: [AppMiddleware(), CalendarMiddleware()]);
+}
