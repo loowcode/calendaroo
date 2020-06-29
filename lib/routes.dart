07d3014e418d828164612dbbc1410bf4e8bd6982
Jacopo Carlini <jack1395@hotmail.it>
2020-06-29 18:58:24 +0200

    removed old pages

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,16 +1,12 @@
-import 'package:calendaroo/pages/add-event/add-event.page.dart';
 import 'package:calendaroo/pages/container.page.dart';
 import 'package:calendaroo/pages/details/datails.page.dart';
 import 'package:calendaroo/pages/settings/settings.page.dart';
-import 'package:calendaroo/pages/show-event/show-event.page.dart';
 import 'package:calendaroo/pages/today/today.page.dart';
 import 'package:flutter/material.dart';
 
 import 'model/event.model.dart';
 
 const HOMEPAGE = '/home';
-const ADD_EVENT = '/add_event';
-const SHOW_EVENT = '/show_event';
 const SETTINGS = '/settings';
 const TODAY = '/today';
 const DETAILS = '/details';
@@ -21,12 +17,6 @@ MaterialPageRoute<dynamic> Function(RouteSettings) routes =
     case HOMEPAGE:
       return MaterialPageRoute(
           builder: (context) => ContainerPage(), settings: settings);
-    case ADD_EVENT:
-      return MaterialPageRoute(
-          builder: (context) => AddEventPage(), settings: settings);
-    case SHOW_EVENT:
-      return MaterialPageRoute(
-          builder: (context) => ShowEventPage(), settings: settings);
     case SETTINGS:
       return MaterialPageRoute(
           builder: (context) => SettingsPage(), settings: settings);
